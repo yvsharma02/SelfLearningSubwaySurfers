15a9c14bd0eb3052e0135e4fcf6f97e0f0d5271c
Yashvardhan Sharma <yvsharma02@gmail.com>
2025-08-30 00:45:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # Arguments:
 # 0: run.sh
-# 1: run or build image
+# 1: run or build or build_and_run image
 # 2: work directory (inside the container)
 # 3: container name
 # 4: image tag
@@ -9,21 +9,50 @@
 export MSYS_NO_PATHCONV=1
 export DOCKER_BUILDKIT=1
 
-MODE=${1:-"run"}
-WORKDIR=${2:-"/home/root/subwaysurfersai/workdir"}
-CONTAINER_NAME=${3:-"subwaysurfersai_container"}
-IMAGE_NAME=${4:-"subwaysurfersai_image"}
-BLOCKING=${5:-"blocking"}
+MODE="build_and_run"
+WORKDIR="/home/root/subwaysurfersai/workdir"
+CONTAINER_NAME="subwaysurfersai_container"
+IMAGE_NAME="subwaysurfersai_image"
+BLOCKING="blocking"
 
-#PIP_CACHE_DIR="$CACHE_DIR/pip"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --mode)
+      MODE="$2"
+      shift 2
+      ;;
+    --workdir)
+      WORKDIR="$2"
+      shift 2
+      ;;
+    --container-name)
+      CONTAINER_NAME="$2"
+      shift 2
+      ;;
+    --image-name)
+      IMAGE_NAME="$2"
+      shift 2
+      ;;
+    --blocking)
+      BLOCKING="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
 
 #This is for runtime
 SHARED_VOLUME="${WORKDIR}/shared"
 
-echo "Building Image..."
-docker build . -t $IMAGE_NAME --build-arg WORKDIR_VAR=$WORKDIR --build-arg SHARED_VOLUME=$SHARED_VOLUME
+if [[ "$MODE" = "build" || "$MODE" = "build_and_run" ]]; then
+    echo "Building Image..."
+    docker build . -t $IMAGE_NAME --build-arg WORKDIR_VAR=$WORKDIR --build-arg SHARED_VOLUME=$SHARED_VOLUME
+fi
 
-if [ "$MODE" = "run" ]; then
+if [[ "$MODE" = "run" || "$MODE" = "build_and_run" ]]; then
     echo "Running Container...."
 
     #TODO: Add check if it is already running.
@@ -44,9 +73,5 @@ if [ "$MODE" = "run" ]; then
         exit 1
     fi
 
-    docker run --rm  --name "$CONTAINER_NAME" --gpus all -v "$(pwd):$SHARED_VOLUME" $IMAGE_NAME $run_flags
-
-elif [ "$MODE" != "build" ]; then
-    echo "Invalid Argument. First argument must be 'build' or 'run'."
-    exit 1
+    docker run --rm --device /dev/kvm --name "$CONTAINER_NAME" --gpus all -v "$(pwd):$SHARED_VOLUME" $IMAGE_NAME $run_flags emulator -avd headlessApi34 -no-window -no-audio -no-boot-anim
 fi
