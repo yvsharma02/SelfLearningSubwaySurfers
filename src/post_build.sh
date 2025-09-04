5ccb3c77246a570b2f37e3fcba0b3dd317496082
Yashvardhan Sharma <yvsharma02@gmail.com>
2025-09-04 23:11:20 +0530

    Minor changes

--- src/post_build.sh
+++ src/post_build.sh
@@ -1,4 +1,4 @@
 # This is only required for development
-apt-get install git
+apt-get update && apt-get install -y git
 git config --global --add safe.directory /home/ubuntu/subwaysurfersai/workspace
 #pip install pure-python-adb opencv-python-headless grpcio grpcio-tools
