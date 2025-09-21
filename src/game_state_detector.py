eef2f6c7edfa77d6eccc4cbb76dde304db81da73
yvsharma02 <yvsharma02@gmail.com>
2025-09-21 09:39:55 +0000

    Better state detector

--- src/game_state_detector.py
+++ src/game_state_detector.py
@@ -5,7 +5,7 @@ from collections import deque
 
 class StateDetector:
 
-    def __init__(self, track_size=10, threshold=5):
+    def __init__(self, track_size=6, detect_bound=0.5, undetect_bound=0.32):
         self.reference_pause = cv2.imread("data/reference_images/pause_button.png")
         self.reference_police = cv2.imread("data/reference_images/police4.png")
         self.reference_doggo = cv2.imread("data/reference_images/doggo2.png")
@@ -14,8 +14,10 @@ class StateDetector:
         self.last_detected = False
 
         self.track_size=track_size
-        self.threshold=threshold
+        self.detect_bound = detect_bound
+        self.undetect_bound = undetect_bound
         self.police_state_queue = deque(maxlen=track_size)
+        self.last_time_detected = None
 
     def detect_gamestate(self,  capture):
         x1, y1, x2, y2 = 11, 10, 66, 67
@@ -27,87 +29,31 @@ class StateDetector:
         return custom_enums.GAME_STATE_OVER
     
     def detector(self, capture, reference):
-        # ROI crop
         x1, y1, x2, y2 = 0, 550, 480, 800
         patch = capture[y1:y2, x1:x2]
-
-        # Template matching
-        cv2.imwrite("ref.png", self.reference_police)
-        cv2.imwrite("patch.png", patch)
-        # cv2.waitKey()
         result = cv2.matchTemplate(patch, reference, cv2.TM_CCOEFF_NORMED)
-
-        # Find best match
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-        # print("Best match score:", max_val)
-        # print("Best location:", max_loc)
-
         return max_val
-        # if max_val > 0.8:   # threshold (tune)
-        #     return True
-        # else:
-        #     print("❌ Feature not found")
-
-
-        # x1, y1, x2, y2 = 0, 400, 480, 800
-        # patch = capture[y1:y2, x1:x2]   # crop ROI from capture
-
-        # # Convert to grayscale (ORB works better on grayscale)
-        # patch_gray = cv2.cvtColor(patch, cv2.COLOR_RGB2GRAY)
-        # ref_gray = cv2.cvtColor(reference, cv2.COLOR_RGB2GRAY)
-
-        # # Initialize ORB
-        # orb = cv2.ORB_create()
-
-        # # Detect keypoints and descriptors
-        # kp1, des1 = orb.detectAndCompute(ref_gray, None)
-        # kp2, des2 = orb.detectAndCompute(patch_gray, None)
-
-        # # detected = False  # default state
-
-        # if des1 is not None and des2 is not None and len(des1) > 0 and len(des2) > 0:
-        #     # Match descriptors using brute-force matcher
-        #     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        #     matches = bf.match(des1, des2)
-
-        #     # Filter good matches (distance < threshold)
-        #     good_matches = [m for m in matches if m.distance < 60]  # tune threshold
-        #     # print(len(good_matches))
-        #     # Decide detection based on number of good matches
-        #     return len(good_matches) # tune threshold
-        # else:
-        #     return 0
-
-        # # State change check
-        # if self.last_detected is None or detected != self.last_detected:
-        #     self.last_detected = detected
-        #     print("Current State: " + ("detected" if detected else "not detected"))
 
     def detect_police(self, capture):
-        # MIN_HOLD = 4
-        THRESHOLD = .45
-        police = self.detector(capture, self.reference_police)
-        # doggo = self.detector(capture, self.reference_doggo)
-        # print(f"police: ${police}, doggo: ${doggo}")
-        self.police_state_queue.append(police)
+        confidence = self.detector(capture, self.reference_police)
+        self.police_state_queue.append(confidence)
+
         if (len(self.police_state_queue) < self.track_size):
             return False
-        detected = False
-        if (sum(self.police_state_queue) / len(self.police_state_queue) > THRESHOLD):
-            detected = True
-            # self.detected_for += 1
-        # else:
-            # self.detected_for = 0
+        
+        avg_confidence = sum(self.police_state_queue) / len(self.police_state_queue)
 
-        return detected
-        # print(self.police_state_queue.count(True))
-        # return self.police_state_queue.count(True) > 6
+        new_detection = None
+        if (avg_confidence > self.detect_bound):
+            new_detection = True
+        if (avg_confidence < self.undetect_bound):
+            new_detection = False
 
-        # detected = False
-        # if (self.detected_for >= MIN_HOLD):
-        #     detected = True
+        if (new_detection is None):
+            new_detection = self.last_detected
+        
+        self.last_detected = new_detection
 
-        # if (self.last_detected == None or self.last_detected != detected):
-        #     self.last_detected = detected
-        #     print("Current State: " + ("detected" if detected else "not detected"))
+        return new_detection
