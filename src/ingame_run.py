f0215a9ab932b5d1e7fd34701c930b56f1ddadab
yvsharma02 <yvsharma02@gmail.com>
2025-10-03 00:15:55 +0000

    I want to cry

--- src/ingame_run.py
+++ src/ingame_run.py
@@ -5,6 +5,13 @@ import torch
 import random
 from collections import deque
 
+
+def log(msg):
+    logfile = open("global_log.txt", "a+")
+    logfile.write(f"{msg}\n")
+    print(msg)
+    
+
 class InGameRun:
     
     class Command:
@@ -43,13 +50,13 @@ class InGameRun:
         if action == constants.ACTION_NOTHING:
             return 0, 0
         if action == constants.ACTION_UP:
-            return 0.1, 1.1 + (random.random() - 0.5) * 2 * .25 # torch.normal(1, .1, size=(1,)).item()
+            return 0.1, 0.8 + (random.random() - 0.5) * 2 * .45
         if action == constants.ACTION_DOWN:
-            return 0.1, 0.65 + (random.random() - 0.5) * 2 * .125 #0.1, torch.normal(0.7, .125, size=(1,)).item()
+            return 0.1, 0.45 + (random.random() - 0.5) * 2 * .125
         if action == constants.ACTION_LEFT:
-            return 0.1, 0.65 + (random.random() - 0.5) * 2 * .125 #0.1, torch.normal(.6, .1, size=(1,)).item()
+            return 0.1, 0.45 + (random.random() - 0.5) * 2 * .125
         if action == constants.ACTION_RIGHT:
-            return 0.1, 0.65 + (random.random() - 0.5) * 2 * .125 #0.1, torch.normal(.6, .1, size=(1,)).item()
+            return 0.1, 0.45 + (random.random() - 0.5) * 2 * .125
     
     def __init__(self, emulator_controller, save_que):
         self.start_time = time.time()
@@ -69,14 +76,14 @@ class InGameRun:
     def give_command(self, action, capture, gamestate, logits):
         if (self.run_secs() < self.start_delay()):
             return
-        
-        if (self.executing_cmd != None and self.executing_cmd.time_since_execution() < self.executing_cmd.elim_win_high):
-            return
 
         now = time.time()
 
         if (action == constants.ACTION_NOTHING):
             self.nothing_buffer.append((action, capture, gamestate, logits, now))
+            return
+        
+        if (self.executing_cmd != None and self.executing_cmd.time_since_execution() < self.executing_cmd.elim_win_high):
             return
         
         win_low, win_high = self.get_command_elim_window(action)
@@ -109,22 +116,28 @@ class InGameRun:
             now = time.time()
             self.flush_nothing_buffer(False, lambda x : (now - x[4]) >= 1 and x[4] <= self.executing_cmd.command_time, debug_log="COMMAND_FLUSH")
             if (self.executing_cmd.time_since_execution() >= self.executing_cmd.elim_win_high and new_state != constants.GAME_STATE_OVER):
+                log("None prev nothing eliminiated (after window): " + str(len([x for x in self.nothing_buffer if x[4] < self.executing_cmd.command_time])))
                 self.flush_nothing_buffer(False, lambda x : (x[4] < self.executing_cmd.command_time), debug_log="AFTER_WINDOW_FLUSH") # TODO: Make sure this eliminates only nothings that happened before the command executed.
                 self.record_cmd(self.executing_cmd, False, "AFTER_WINDOW")
                 self.executing_cmd = None
             elif (new_state == constants.GAME_STATE_OVER):
                 if (self.executing_cmd.time_since_execution() < self.executing_cmd.elim_win_low):
-                    print("All prev nothing eliminiated: " + str(len([x for x in self.nothing_buffer if x[4] < self.executing_cmd.command_time])))
+                    log("All prev nothing eliminiated: " + str(len([x for x in self.nothing_buffer if x[4] < self.executing_cmd.command_time])))
                     self.flush_nothing_buffer(True, lambda x : (x[4] < self.executing_cmd.command_time), debug_log="BEFORE_WINDOW_FLUSH") # Elimninate last few seconds of noting.
                     # self.record_cmd(self.executing_cmd, False, "BEFORE_WINDOW") #Just don't bother with this.
                 elif (self.executing_cmd.elim_win_low <= self.executing_cmd.time_since_execution() and self.executing_cmd.time_since_execution() <= self.executing_cmd.elim_win_high):
-                    print("None prev nothing eliminiated: " + str(len([x for x in self.nothing_buffer if x[4] < self.executing_cmd.command_time])))
+                    log("None prev nothing eliminiated: " + str(len([x for x in self.nothing_buffer if x[4] < self.executing_cmd.command_time])))
                     self.flush_nothing_buffer(False, lambda x : (x[4] < self.executing_cmd.command_time), debug_log="IN_WINDOW_FLUSH")
                     self.record_cmd(self.executing_cmd, True, "IN_WINDOW")
-                
+                else:
+                    log("All prev nothing eliminiated (Lost Condition): " + str(len([x for x in self.nothing_buffer if x[4] >= self.executing_cmd.command_time + self.executing_cmd.elim_win_high])))
+                    self.flush_nothing_buffer(True, lambda x : (x[4] >= self.executing_cmd.command_time + self.executing_cmd.elim_win_high), debug_log="LOST_CONDITION")            
                 self.executing_cmd = None
+            else:
+                log("???????????????????")
+            
         else:
-            print("No pending command")
+            log("No pending command")
             now = time.time()
             self.flush_nothing_buffer(False, lambda x : (now - x[4]) >= 1, debug_log="NO_COMMAND_FLUSH_NON_ELIM")
             if (new_state == constants.GAME_STATE_OVER):
@@ -156,7 +169,7 @@ class InGameRun:
         _, act_min_idx = torch.min(logits, dim=0)
         act_max = constants.action_to_name(act_max_idx.item())
         act_min = constants.action_to_name(act_min_idx.item())
-        print("Eliminated: [" + ",".join([constants.action_to_name(x) for x in eliminations]) + "] ; Logits: [" + ", ".join([f'{x:.4f}' for x in logits]) + "]; " + "ELIM_MAX: " + act_max + "; ELIM_MIN: " + act_min + "; DEBUG: " + debug_log)
+        log("Eliminated: [" + ",".join([constants.action_to_name(x) for x in eliminations]) + "] ; Logits: [" + ", ".join([f'{x:.4f}' for x in logits]) + "]; " + "ELIM_MAX: " + act_max + "; ELIM_MIN: " + act_min + "; DEBUG: " + debug_log)
         self.save_que.put(eliminations, capture, cmd_time, logits, debug_log)
 
     def execute_command(self, cmd : Command):
