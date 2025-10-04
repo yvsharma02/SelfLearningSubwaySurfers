c714ec7d333b542b003a49555e9e87480de049fe
yvsharma02 <yvsharma02@gmail.com>
2025-10-04 22:01:41 +0000

    Tweaking trainer.py

--- src/trainer.py
+++ src/trainer.py
@@ -16,13 +16,12 @@ import shutil
 from PIL import Image
 import pipeline
 
-# One in X
-MULTI_ELIM_PERCENTAGE_OF_SINGLE_ELIM = 1000000
-MULTI_ELIM_NOTHING_LIMIT = 0.666666 # This percent of single elim can be nothing multi elims
+MULTI_ELIM_PERCENTAGE_OF_SINGLE_ELIM = 8
+MULTI_ELIM_NOTHING_LIMIT = 0.475 # This percent of single elim can be nothing multi elims
 
 # Returns (img_path, label (tensor with length 5 denoting elimination confidence.))
 def read_data(path):
-    res_mp = {
+    single_elim = {
 
     }
     multi_elim = {
@@ -51,17 +50,21 @@ def read_data(path):
                     if (len(eliminations) > 1):
                         multi_elim[imname] = label
                     else: 
-                        res_mp[imname] = label
+                        single_elim[imname] = label
+
+    res_mp = {}
 
     no_action_multi_elims = [k for k in multi_elim.keys() if multi_elim[k][0] < 0.25]
     action_multi_elims = [k for k in multi_elim.keys() if k not in no_action_multi_elims]
-    sampled_no_actions = np.random.choice(no_action_multi_elims, size=min(int(len(res_mp.keys()) * MULTI_ELIM_PERCENTAGE_OF_SINGLE_ELIM * MULTI_ELIM_NOTHING_LIMIT), len(no_action_multi_elims)), replace=False)
-    sampled_actions = np.random.choice(action_multi_elims, size=min(int(len(res_mp.keys()) * MULTI_ELIM_PERCENTAGE_OF_SINGLE_ELIM) - len(sampled_no_actions), len(action_multi_elims.keys())), replace=False)
+    sampled_no_actions = np.random.choice(no_action_multi_elims, size=min(int(len(single_elim.keys()) * MULTI_ELIM_PERCENTAGE_OF_SINGLE_ELIM * MULTI_ELIM_NOTHING_LIMIT), len(no_action_multi_elims)), replace=False)
+    sampled_actions = np.random.choice(action_multi_elims, size=min(int(len(single_elim.keys()) * MULTI_ELIM_PERCENTAGE_OF_SINGLE_ELIM) - len(sampled_no_actions), len(action_multi_elims)), replace=False)
 
     for k in sampled_actions:
-        res_mp[k] = multi_elim[k]
+        single_elim[k] = multi_elim[k]
     for k in sampled_no_actions:
-        res_mp[k] = multi_elim[k]
+        single_elim[k] = multi_elim[k]
+    for k in single_elim.keys():
+        res_mp[k] = single_elim[k]
 
     return res_mp
 
@@ -90,11 +93,11 @@ def create_datasets(train_paths, test_paths, train_labels, test_labels, transfor
 
     return train_dataset, train_loader, test_dataset, test_loader
 
-def train(model, train_loader, device):
+def train(model, train_loader, test_loader, device):
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-    num_epochs = 50
-
-    for epoch in range(num_epochs):
+    MAX_EPOCH = 50
+    test_losses = []
+    for epoch in range(MAX_EPOCH):
         model.train()
         running_loss = 0.0
 
@@ -136,11 +139,19 @@ def train(model, train_loader, device):
             single_elim_acc = f"{((single_elim_correct) / (single_elim_total)):.4f}" if single_elim_total > 0 else "NA"
             multi_elim_acc = f"{((multi_elim_correct) / (multi_elim_total)):.4f}" if multi_elim_total > 0 else "NA"
             total_acc = (single_elim_correct + multi_elim_correct) / (single_elim_total + multi_elim_total)
-            print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {train_loss:.4f}, Accuracy: {total_acc:.4f}, Single Elim Accuracy: {single_elim_acc}, Multi Elim Accuracy: {multi_elim_acc}")
+            print(f"Epoch [{epoch+1}/{MAX_EPOCH}], Loss: {train_loss:.4f}, Accuracy: {total_acc:.4f}, Single Elim Accuracy: {single_elim_acc}, Multi Elim Accuracy: {multi_elim_acc}")
         else:
-            print(f"Empty Empoch: {epoch+1}/{num_epochs}")
+            print(f"Empty Empoch: {epoch+1}/{MAX_EPOCH}")
 
-    model.save_to_file("generated/models/test.pth")
+        model.save_to_file("generated/models/test.pth")
+        test_loss = test(model, test_loader, device)[0]
+        window = test_loss[-max(len(test_loss, 7)):]
+        avg_loss = 0 if len(window) == 0 else sum(window) / len(window)
+        test_loss.append(test_loss)
+
+        if (len(window) > 7 and avg_loss - test_loss < 0.001):
+            break
+    print("Training Finished!")
 
 def test(model, test_loader, device):
     model.eval()
@@ -212,8 +223,8 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
     model = SSAIModel().to(device)
-    train(model, train_loader, device)
-    test(model, test_loader, device)
+    train(model, train_loader, test_loader, device)
+    # test(model, test_loader, device)
 
 
 if __name__ == "__main__":
