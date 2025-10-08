bb4241f676abf87ecf8a321acd36c77c0f3118b9
yvsharma02 <yvsharma02@gmail.com>
2025-10-08 12:55:00 +0000

    Fixed setup/launch.sh

--- setup/launch.sh
+++ setup/launch.sh
@@ -1,8 +1,10 @@
 adb devices
 
-export ANDROID_EMULATOR_USE_SYSTEM_LIBS=1
+# export ANDROID_EMULATOR_USE_SYSTEM_LIBS=1
 
-Xvfb :1 -screen 0 1080x2340x24 &
-export DISPLAY=:1
+# Xvfb :1 -screen 0 1080x2340x24 &
+# export DISPLAY=:1
 
-python src/main.py
+sleep 10
+
+python src/player.py
